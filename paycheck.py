0aa9bf655623ed0876caaa5e04a87b32f93fb7a9
Alannah <imalannahmarie@gmail.com>
2018-11-14 18:59:47 -0800

    Update paycheck.py
    Cleaned up code. Defined two separate functions to calculate regular and overtime pay. Want to upgrade program in the future to calculate weekly, bi-monthly and monthly paycheck.

--- paycheck.py
+++ paycheck.py
@@ -1,15 +1,14 @@
 """
 paycheck.py
-Calculates a user's paycheck based on information entered by the user:
+Calculates a user's weekly paycheck based on information entered by the user:
 1. Hourly Rate
 2. Hours Worked
 3. Overtime Rate
 """
 
-# Import Decimal Library
+# Import decimal library
 from decimal import Decimal
 
-# Converts input string to Decimal
 pay_rate = input("What's your hourly rate in US dollars?\n")
 hours = input("How many hours did you work this week?\n")
 overtime_rate = input("What is your overtime rate?\n")
@@ -20,21 +19,30 @@ try:
     hours = Decimal(hours)
     overtime_rate = Decimal(overtime_rate)
 
-    # Calculates overtime pay
-    if hours > 40:
-        regular_hours = 40
+    # 40 hour work week
+    regular_hours = 40
+
+    # Defined two separate functions to calculate regular and overtime pay
+    def regularPay():
+        paycheck = pay_rate * hours
+        print(paycheck)
+        return paycheck
+
+    def overtimePay():
         overtime_hours = hours - regular_hours
         overtime_pay = overtime_hours * pay_rate * overtime_rate
-        # Adds overtime payrate to paycheck
-        paycheck = (pay_rate * regular_hours) + overtime_pay
+        # Adds overtime pay to paycheck
+        overtime_check = (pay_rate * regular_hours) + overtime_pay
         # Rounds paycheck to two decimal points
-        paycheck = round(paycheck, 2)
-        print("This week you'll be paid: $%s" % (paycheck))
-    # Calculates regular pay
+        overtime_check = round(overtime_check, 2)
+        print(overtime_check)
+        return overtime_check
+
+    if hours <= regular_hours:
+        regularPay()
     else:
-        paycheck = pay_rate * hours
-        # Rounds paycheck to two decimal points
-        paycheck = round(paycheck, 2)
-        print("This week you'll be paid: $%s" % (paycheck))
+        overtimePay()
+
 except:
-    print("Please enter a number.")
+    # Prints when user enters a non-numerical input
+    print("Please enter a number unless you're prompted otherwise.")
